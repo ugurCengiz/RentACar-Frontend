58e8c5477a681f87df1f1d5b275ea10dab37754d
uğur cengiz <ugur_nobre@hotmail.com>
2022-07-23 17:02:19 +0300

    23.07.22 finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,37 +23,35 @@ const routes: Routes = [
   {path:"",pathMatch:"full",component:CarDtoComponent},
 
   {path:"cars",component:CarDtoComponent},
-  {path:"colors",component:CarDtoComponent},
+  {path:"colors",component:CarDtoComponent,canActivate:[LoginGuard]},
   {path:"cars/brand/:brandId",component:CarDtoComponent}, 
   {path:"cars/color/:colorId",component:CarDtoComponent},  
-  {path:"cars/cardetail/:carId",component:CarDetailComponent},
+  {path:"cars/cardetail/:carId",component:CarDetailComponent,canActivate:[LoginGuard]},
   {path:"cars/filter/:brandId/:colorId",component:CarDtoComponent},
   {path:"cars/add",component:CarAddComponent,canActivate:[LoginGuard]},
+  {path:"cars/update/:carId",component:CarUpdateComponent,canActivate:[LoginGuard]},
 
-  {path:"carlist",component:CarListComponent},
-  {path:"carlist/car/:carId",component:CarUpdateComponent},
+  {path:"carlist",component:CarListComponent,canActivate:[LoginGuard]},
+  {path:"carlist/car/:carId",component:CarUpdateComponent,canActivate:[LoginGuard]},
 
-  {path:"brands",component:BrandListComponent},
-  {path:"brands/add",component:BrandAddComponent},
-  {path:"brands/update/:brandId",component:CarUpdateComponent},
+  {path:"brands",component:BrandListComponent,canActivate:[LoginGuard]},
+  {path:"brands/add",component:BrandAddComponent,canActivate:[LoginGuard]},
+  {path:"brands/update/:brandId",component:CarUpdateComponent,canActivate:[LoginGuard]},
 
-  {path:"rentals",component:RentalDtoComponent},
-  {path:"rentals/car/:carId",component:RentalAddComponent},
-  {path:"rentals/add",component:RentalAddComponent},
 
-  {path:"colors",component:ColorListComponent},
-  {path:"colors/add",component:ColorAddComponent},
-  {path:"colors/update/:colorId",component:ColorUpdateComponent},
+  {path:"colors",component:ColorListComponent,canActivate:[LoginGuard]},
+  {path:"colors/add",component:ColorAddComponent,canActivate:[LoginGuard]},
+  {path:"colors/update/:colorId",component:ColorUpdateComponent,canActivate:[LoginGuard]},
 
   {path:"login",component:LoginComponent},
   {path:"register",component:RegisterComponent},
   
-  {path:"rentals",component:RentalDtoComponent},
-  {path:"rentals/car/:carId",component:RentalAddComponent},
-  {path:"rentals/add",component:RentalAddComponent},
+  {path:"rentals",component:RentalDtoComponent,canActivate:[LoginGuard]},
+  {path:"rentals/car/:carId",component:RentalAddComponent,canActivate:[LoginGuard]},
+  {path:"rentals/add",component:RentalAddComponent,canActivate:[LoginGuard]},
 
-  {path:"payment",component:PaymentComponent},
-  {path:"payment/car/:carId",component:PaymentComponent},
+  {path:"payment",component:PaymentComponent,canActivate:[LoginGuard]},
+  {path:"payment/car/:carId",component:PaymentComponent,canActivate:[LoginGuard]},
 ];
 
 @NgModule({
